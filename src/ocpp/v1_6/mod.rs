456c5beb3acedb9733a8f45070321bea6502d638
hlsxx <holespato@gmail.com>
2025-05-21 13:03:56 +0200

    (feat) v1.6 types (action)

--- src/ocpp/v1_6/mod.rs
+++ src/ocpp/v1_6/mod.rs
@@ -1,2 +1,3 @@
 pub mod message_generator;
 pub mod simulator;
+pub mod types;
---
+++ src/ocpp/v1_6/types.rs
@@ -0,0 +1,117 @@
+use std::str::FromStr;
+
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub enum OcppAction {
+  // CP → CSMS
+  BootNotification,
+  Heartbeat,
+  Authorize,
+  StartTransaction,
+  StopTransaction,
+  StatusNotification,
+  MeterValues,
+  DiagnosticsStatusNotification,
+  FirmwareStatusNotification,
+  DataTransfer,
+
+  // CSMS → CP
+  RemoteStartTransaction,
+  RemoteStopTransaction,
+  Reset,
+  ChangeAvailability,
+  ChangeConfiguration,
+  GetConfiguration,
+  ClearCache,
+  UpdateFirmware,
+  GetDiagnostics,
+  UnlockConnector,
+  CancelReservation,
+  ReserveNow,
+  SetChargingProfile,
+  ClearChargingProfile,
+  GetCompositeSchedule,
+  GetLocalListVersion,
+  SendLocalList,
+}
+
+impl FromStr for OcppAction {
+  type Err = &'static str;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    use OcppAction::*;
+
+    match s {
+      "BootNotification" => Ok(BootNotification),
+      "Heartbeat" => Ok(Heartbeat),
+      "Authorize" => Ok(Authorize),
+      "StartTransaction" => Ok(StartTransaction),
+      "StopTransaction" => Ok(StopTransaction),
+      "StatusNotification" => Ok(StatusNotification),
+      "MeterValues" => Ok(MeterValues),
+      "DiagnosticsStatusNotification" => Ok(DiagnosticsStatusNotification),
+      "FirmwareStatusNotification" => Ok(FirmwareStatusNotification),
+      "DataTransfer" => Ok(DataTransfer),
+
+      "RemoteStartTransaction" => Ok(RemoteStartTransaction),
+      "RemoteStopTransaction" => Ok(RemoteStopTransaction),
+      "Reset" => Ok(Reset),
+      "ChangeAvailability" => Ok(ChangeAvailability),
+      "ChangeConfiguration" => Ok(ChangeConfiguration),
+      "GetConfiguration" => Ok(GetConfiguration),
+      "ClearCache" => Ok(ClearCache),
+      "UpdateFirmware" => Ok(UpdateFirmware),
+      "GetDiagnostics" => Ok(GetDiagnostics),
+      "UnlockConnector" => Ok(UnlockConnector),
+      "CancelReservation" => Ok(CancelReservation),
+      "ReserveNow" => Ok(ReserveNow),
+      "SetChargingProfile" => Ok(SetChargingProfile),
+      "ClearChargingProfile" => Ok(ClearChargingProfile),
+      "GetCompositeSchedule" => Ok(GetCompositeSchedule),
+      "GetLocalListVersion" => Ok(GetLocalListVersion),
+      "SendLocalList" => Ok(SendLocalList),
+
+      _ => Err("Unknown OCPP v1.6 action"),
+    }
+  }
+}
+
+impl std::fmt::Display for OcppAction {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use OcppAction::*;
+
+    let s = match self {
+      BootNotification => "BootNotification",
+      Heartbeat => "Heartbeat",
+      Authorize => "Authorize",
+      StartTransaction => "StartTransaction",
+      StopTransaction => "StopTransaction",
+      StatusNotification => "StatusNotification",
+      MeterValues => "MeterValues",
+      DiagnosticsStatusNotification => "DiagnosticsStatusNotification",
+      FirmwareStatusNotification => "FirmwareStatusNotification",
+      DataTransfer => "DataTransfer",
+      RemoteStartTransaction => "RemoteStartTransaction",
+      RemoteStopTransaction => "RemoteStopTransaction",
+      Reset => "Reset",
+      ChangeAvailability => "ChangeAvailability",
+      ChangeConfiguration => "ChangeConfiguration",
+      GetConfiguration => "GetConfiguration",
+      ClearCache => "ClearCache",
+      UpdateFirmware => "UpdateFirmware",
+      GetDiagnostics => "GetDiagnostics",
+      UnlockConnector => "UnlockConnector",
+      CancelReservation => "CancelReservation",
+      ReserveNow => "ReserveNow",
+      SetChargingProfile => "SetChargingProfile",
+      ClearChargingProfile => "ClearChargingProfile",
+      GetCompositeSchedule => "GetCompositeSchedule",
+      GetLocalListVersion => "GetLocalListVersion",
+      SendLocalList => "SendLocalList",
+    };
+
+    write!(f, "{s}")
+  }
+}
+
